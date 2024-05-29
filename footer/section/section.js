4d72b2d4561467ed3e7960673bad8acaeb991dab
Iskrisss <klfpaz@scl.edu.gt>
2024-05-28 20:12:29 -0600

    git actualizado

---
+++ footer/section/section.js
@@ -0,0 +1,2 @@
+let footer = document.querySelector(".section");
+footer.innerHTML = ' <div class="block">Bloque 1</div><div class="block">Bloque 2</div><div class="block">Bloque 3</div><div class="block">Bloque 4</div><div class="block">Bloque 5</div>'
